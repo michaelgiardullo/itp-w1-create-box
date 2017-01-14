92de9b4e06691c2678fab2eaf5faea3137094a8a
Michael Giardullo <michaelgiardullo@gmail.com>
2017-01-14 16:01:49 +0000

    Create Box Program

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,21 @@
 """This is the entry point of the program."""
 
 
-def create_box(height, width, character):
-    pass
-
+def create_box(height,width,character):
+    height_count = 0
+    boxstring=""
+    if height==0 and width==0:
+            return ("invalid measurements")
+    while True:
+        if height_count < height:
+            for w in range(width):
+                boxstring = boxstring + character
+            height_count = height_count + 1
+            if height_count <= height:
+                boxstring = boxstring + '\n'
+        else:
+            break
+    return boxstring
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
